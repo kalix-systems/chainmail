a42470c2f6f1253b4ec8070db2f49be24a421dce
theseriousadult <jack@kalix.io>
2019-09-17 22:14:49 -0700

    rearrange Error type slightly

--- src/errors.rs
+++ src/errors.rs
@@ -3,9 +3,9 @@ use std::{error, fmt};
 #[derive(Eq, PartialEq, Debug)]
 pub enum Error {
     CryptoError,
-    MissingKeys,
     DecryptionError,
     BadSig,
+    MissingKeys,
     BlockStoreSaveError,
     BlockStoreMarkError,
 }
@@ -16,9 +16,9 @@ impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match self {
             CryptoError => write!(fmt, "libsodium failed"),
-            MissingKeys => write!(fmt, "BlockStore couldn't find keys"),
             DecryptionError => write!(fmt, "Failed to decrypt a block"),
             BadSig => write!(fmt, "Signature did not sign data"),
+            MissingKeys => write!(fmt, "BlockStore couldn't find keys"),
             BlockStoreSaveError => write!(fmt, "BlockStore couldn't save key"),
             BlockStoreMarkError => write!(fmt, "BlockStore couldn't mark key as unused"),
         }
